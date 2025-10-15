0bcb7d0d639d46ab8dd1d705758d4b6b371917e3
lwj <jg15575446613@163.com>
2025-10-15 11:26:21 +0800

    feat(clone): clone函数

--- clone/index.js
+++ clone/index.js
@@ -1,8 +1,28 @@
 function clone(obj) {
   const valueType = Object.prototype.toString.call(obj).slice(8, -1)
 
+  // 这个赋值是为了克隆基础类型数据
   let result = obj
 
+  // 克隆Set
+  if (valueType === 'Set') {
+    return new Set([...obj].map(value => clone(value)))
+  }
+
+  // 克隆Map
+  if (valueType === 'Map') {
+    return new Map([...obj].map(([k, v]) => [k, v]))
+  }
+
+  // 克隆Date
+  if (valueType === 'Date') {
+    result = new Date(obj.getTime())
+  }
+
+
+  // 克隆RegExp
+
+  // 克隆数组或者对象
   if (valueType === 'Array' || valueType === 'Object') {
     result = Array.isArray(obj) ? [] : {}
     for (let k in obj) {
@@ -13,15 +33,22 @@ function clone(obj) {
   return result
 }
 
+// var obj = {
+//   a: 1,
+//   b: 2,
+//   c: [
+//     3,
+//     {
+//       d: 4
+//     }
+//   ]
+// }
 var obj = {
-  a: 1,
-  b: 2,
-  c: [
-    3,
-    {
-      d: 4
-    }
-  ]
+  // a: new Date()
+  // a: new Set([1])
+  a: new Map([
+    ['a', 1]
+  ])
 }
 var obj2 = clone(obj)
 
