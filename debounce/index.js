a8421629fcc2040bde73a1720712db1473abe913
lwj <jg15575446613@163.com>
2025-11-04 19:34:34 +0800

    feat: deounce学习

--- debounce/index.js
+++ debounce/index.js
@@ -5,49 +5,72 @@
 // cancel 直接调用函数 取消函数
 // flush，直接调用函数，立即执行
 
+// 分析的时候应该知道每一步的时候，作用域内有哪些变量可用
+// 这样就能更好的知道，用这些变量来做一些事情，或者对它进行清理
 
-function debounce(fn, wait, canFirst) {
-  let timeout = null
-  let debouncedFn = null
+// 接收三个参数
+// fn 防抖函数
+// wait 防抖的时间
+// callFirst 是否需要立即执行一次
+function debounce(fn, wait, callFirst) {
 
-  const clear = function () {
-    clearTimeout(timeout)
-    timeout = null
-    debouncedFn = null
-  }
+  // 
+  var timeout = null;
+  var debouncedFn = null;
 
-  const flush = function () {
-    clear()
-    debouncedFn()
-  }
+  var clear = function () {
+    if (timeout) {
+      clearTimeout(timeout);
 
-  const debounceWrapper = function (...args) {
-    const context = this
+      debouncedFn = null;
+      timeout = null;
+    }
+  };
 
-    if (canFirst) {
-      fn.call(context, ...args)
-      canFirst = false
-      return
+  var flush = function () {
+    var call = debouncedFn;
+    clear();
+
+    if (call) {
+      call();
+    }
+  };
+
+  var debounceWrapper = function () {
+    if (!wait) {
+      return fn.apply(this, arguments);
     }
 
-    clear()
+    var context = this;
+    var args = arguments;
+    var callNow = callFirst && !timeout;
+    clear();
 
     debouncedFn = function () {
-      fn.call(context, ...args)
-    }
+      fn.apply(context, args);
+    };
 
-    timeout = setTimeout(() => {
-      debouncedFn()
+    timeout = setTimeout(function () {
+      timeout = null;
+
+      if (!callNow) {
+        var call = debouncedFn;
+        debouncedFn = null;
+
+        return call();
+      }
     }, wait);
 
-  }
+    if (callNow) {
+      return debouncedFn();
+    }
+  };
 
-  debounceWrapper.cancel = clear
-  debounceWrapper.flush = flush
+  debounceWrapper.cancel = clear;
+  debounceWrapper.flush = flush;
 
-  return debounceWrapper
+  return debounceWrapper;
 }
-
 const fn = debounce(function (a) {
   console.log('debounce', a)
 }, 500)
